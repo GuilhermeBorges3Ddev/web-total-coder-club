d3c6b7c2fdf832ec0ddcdb20963c6d92018af4e6
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-07-29 01:59:07 -0300

    File explainning JS internal organization was done

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,16 @@
+//Exibição de string usando aspas duplas
+console.log("Sentença de código")
+
+//Exibição de string usando aspas simples
+console.log('Sou uma nova sentença...');
+
+//Backtip ou template-string
+let dataHoje = new Date();
+console.log(`Hoje estamos em ----> ${dataHoje}` + " <-----");
+
+//Bloco de código (agrupa sentenças) 
+{
+    {
+        console.log(`Aspas dentro das aspas: ""`)
+    }
+}
