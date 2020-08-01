61e31fdd9c2bb447dc0846260c440d14d6688757
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-07-31 23:54:10 -0300

    Testing type of a let variable what recieved Date() into file organization.js

--- fundamentos/organizacao.js
+++ fundamentos/organizacao.js
@@ -1,12 +1,19 @@
 //Exibição de string usando aspas duplas
 console.log("Sentença de código")
+console.log("---------------------------------------------------")
 
 //Exibição de string usando aspas simples
 console.log('Sou uma nova sentença...');
+console.log("---------------------------------------------------")
 
 //Backtip ou template-string
 let dataHoje = new Date();
 console.log(`Hoje estamos em ----> ${dataHoje}` + " <-----");
+console.log("---------------------------------------------------")
+
+//Verificando o tipo de uma data - 'Date'
+console.log(typeof dataHoje)
+console.log("---------------------------------------------------")
 
 //Bloco de código (agrupa sentenças) 
 {
